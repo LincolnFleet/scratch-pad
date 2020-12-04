f87de312c51409daa6f776f9d2abeb32a1339f2f
Travis Martin <travisemerymartin@gmail.com>
2020-12-04 04:04:12 -0500

    solution 2 progress

--- advent-of-code/2020/day4/solution.js
+++ advent-of-code/2020/day4/solution.js
@@ -10,8 +10,50 @@ function part1(data, requiredFields, optionalFields) {
         return acc;
     }, 0);
 }
-var part1RequiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-var part1OptionalFields = ["cid"];
+// const fieldTitleRGX: RegExp = RegExp(`${Object.keys(requiredFields).map((field) => `(${field}:)`).join("|")}`, "g");
+// const fieldTitles: Set<string> = new Set(Object.keys(docObj));
+function part2(data, requiredFields, optionalFields) {
+    return data.reduce(function (acc, doc) {
+        var docObj = doc.split("/( |\n)/gm").reduce(function (acc, line) {
+            var tup = line.split(":");
+            acc[tup[0]] = tup[1];
+            return acc;
+        }, {});
+        for (var _i = 0, docObj_1 = docObj; _i < docObj_1.length; _i++) {
+            var key = docObj_1[_i];
+            if (requiredFields[key](docObj[key])) {
+                return acc + 1;
+            }
+        }
+        return acc;
+    }, 0);
+}
+var part1Required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+var part1Optional = ["cid"];
 console.time("part 1");
-console.log(part1(DATA, part1RequiredFields, part1OptionalFields));
+console.log(part1(DATA, part1Required, part1Optional));
 console.timeLog("part 1");
+var part2Required = {
+    byr: function (str) { return parseInt(str, 10) >= 1920 && parseInt(str, 10) <= 2002; },
+    iyr: function (str) { return parseInt(str, 10) >= 2010 && parseInt(str, 10) <= 2020; },
+    eyr: function (str) { return parseInt(str, 10) >= 2020 && parseInt(str, 10) <= 2030; },
+    hgt: function (str) {
+        var valid = str.match("/^(d{2,3})(cm|in)$/g");
+        if (valid) {
+            if (valid[1] == "in") {
+                return parseInt(valid[0], 10) >= 59 && parseInt(valid[0], 10) <= 76;
+            }
+            else if (valid[1] == "cm") {
+                return parseInt(valid[0], 10) >= 150 && parseInt(valid[0], 10) <= 193;
+            }
+        }
+        return false;
+    },
+    hcl: function (str) { return str.test("/^#([a-f]|[0-9]){6}$/g"); },
+    ecl: function (str) { return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(str); },
+    pid: function (str) { return str.test("/^d{9}$/g"); }
+};
+var part2Optional = { cid: function (str) { return true; } };
+console.time("part 2");
+console.log(part2(DATA, part2Required, part2Optional));
+console.timeLog("part 2");
--- advent-of-code/2020/day4/solution.ts
+++ advent-of-code/2020/day4/solution.ts
@@ -14,9 +14,62 @@ function part1(data: string[], requiredFields: string[], optionalFields: string[
   }, 0);
 }
 
-const part1RequiredFields: Array<string> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-const part1OptionalFields: Array<string> = ["cid"];
+// const fieldTitleRGX: RegExp = RegExp(`${Object.keys(requiredFields).map((field) => `(${field}:)`).join("|")}`, "g");
+// const fieldTitles: Set<string> = new Set(Object.keys(docObj));
+
+function part2(data: string[], requiredFields: {}, optionalFields: {}) {
+  const validPassportCount = data.reduce((acc1, doc) => {
+    const numberOfRequiredFields: number = Object.keys(requiredFields);
+    const lines: string[] = doc.split("/( |\n)/gm");
+
+    if (numberOfRequiredFields == lines.length) {
+      
+      const passingFields: number = lines.reduce((acc2, line) => {
+        const [key, value] = line.split(":");
+        if (requiredFields[key](value)) {
+          return acc2 + 1;
+        }
+      }, 0);
+
+      if (passingFields == numberOfRequiredFields) {
+        return acc1 + 1;
+      }
+    }
+
+    return acc1;
+  }, 0);
+
+  return validPassportCount;
+}
+
+const part1Required: Array<string> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+const part1Optional: Array<string> = ["cid"];
 
 console.time("part 1");
-console.log(part1(DATA, part1RequiredFields, part1OptionalFields));
+console.log(part1(DATA, part1Required, part1Optional));
 console.timeLog("part 1");
+
+const part2Required = {
+  byr: (str) => parseInt(str, 10) >= 1920 && parseInt(str, 10) <= 2002,
+  iyr: (str) => parseInt(str, 10) >= 2010 && parseInt(str, 10) <= 2020,
+  eyr: (str) => parseInt(str, 10) >= 2020 && parseInt(str, 10) <= 2030,
+  hgt: (str) => {
+    const valid = str.match("/^(d{2,3})(cm|in)$/g");
+    if (valid) {
+      if (valid[1] == "in") {
+        return parseInt(valid[0], 10) >= 59 && parseInt(valid[0], 10) <= 76;
+      } else if (valid[1] == "cm") {
+        return parseInt(valid[0], 10) >= 150 && parseInt(valid[0], 10) <= 193;
+      }
+    }
+    return false;
+  },
+  hcl: (str) => str.test("/^#([a-f]|[0-9]){6}$/g"),
+  ecl: (str) => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].includes(str),
+  pid: (str) => str.test("/^d{9}$/g"),
+};
+const part2Optional = { cid: (str) => true };
+
+console.time("part 2");
+console.log(part2(DATA, part2Required, part2Optional));
+console.timeLog("part 2");
