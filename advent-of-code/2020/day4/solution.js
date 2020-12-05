db6063de8580cf1fb08cbd625278a6bb9cbf1192
Travis Martin <travisemerymartin@gmail.com>
2020-12-05 14:43:31 -0500

    merge parts to single fn

--- advent-of-code/2020/day4/solution.js
+++ advent-of-code/2020/day4/solution.js
@@ -1,28 +1,32 @@
 #!usr/bin/env node
 var fs = require("fs");
 var DATA = fs.readFileSync("./input.txt", "utf-8").split("\n\n");
-function part1(data, requiredFields, optionalFields) {
-    var rgx = RegExp("" + requiredFields.map(function (field) { return "(" + field + ":)"; }).join("|"), "gm");
-    return data.reduce(function (acc, doc) {
-        if (doc.match(rgx).length >= requiredFields.length) {
-            return acc + 1;
-        }
-        return acc;
-    }, 0);
-}
-// const fieldTitleRGX: RegExp = RegExp(`${Object.keys(requiredFields).map((field) => `(${field}:)`).join("|")}`, "g");
-function part2(data, requiredFields, optionalFields) {
+// function part1(data: string[], requiredFields: string[], optionalFields: string[]) {
+//   const rgx: RegExp = RegExp(`${requiredFields.map((field) => `(${field}:)`).join("|")}`, "gm");
+//   return data.reduce((acc, doc) => {
+//     if (doc.match(rgx).length >= requiredFields.length) {
+//       return acc + 1;
+//     }
+//     return acc;
+//   }, 0);
+// }
+function scanDocs(data, requiredFields, optionalFields, ignoreFieldValues) {
+    if (optionalFields === void 0) { optionalFields = {}; }
+    if (ignoreFieldValues === void 0) { ignoreFieldValues = false; }
     var requiredFieldKeys = Object.keys(requiredFields);
     var optionalFieldKeys = Object.keys(optionalFields);
     var validPassportCount = data.reduce(function (acc1, doc, i) {
-        console.log("doc:", i);
-        console.log(doc);
-        var lines = doc.split(/ |\n/g);
-        if (requiredFieldKeys.length == lines.length || requiredFieldKeys.length + optionalFieldKeys.length == lines.length) {
+        // console.log("doc:", i);
+        // console.log(doc);
+        var lines = doc.split(/ |\n/g).map(function (line) { return line.split(":"); });
+        if (requiredFieldKeys.length == lines.length ||
+            requiredFieldKeys.length + optionalFieldKeys.length == lines.length) {
             var passingFieldsCount = lines.reduce(function (acc2, line) {
-                var _a = line.split(":"), key = _a[0], value = _a[1];
-                // console.log(key, value);
-                if (requiredFieldKeys.includes(key) && requiredFields[key](value)) {
+                var key = line[0], value = line[1];
+                if (ignoreFieldValues && requiredFieldKeys.includes(key)) {
+                    return acc2 + 1;
+                }
+                else if (requiredFieldKeys.includes(key) && requiredFields[key](value)) {
                     return acc2 + 1;
                 }
                 else {
@@ -30,20 +34,17 @@ function part2(data, requiredFields, optionalFields) {
                 }
             }, 0);
             if (passingFieldsCount == requiredFieldKeys.length) {
-                console.log("pass");
+                // console.log("pass");
                 return acc1 + 1;
             }
         }
-        console.log("fail");
+        // console.log("fail");
         return acc1;
     }, 0);
     return validPassportCount;
 }
-var part1Required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-var part1Optional = ["cid"];
-console.time("part 1");
-console.log(part1(DATA, part1Required, part1Optional));
-console.timeLog("part 1");
+// const part1Required: Array<string> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+// const part1Optional: Array<string> = ["cid"];
 var part2Required = {
     byr: function (str) { return parseInt(str, 10) >= 1920 && parseInt(str, 10) <= 2002; },
     iyr: function (str) { return parseInt(str, 10) >= 2010 && parseInt(str, 10) <= 2020; },
@@ -65,6 +66,9 @@ var part2Required = {
     pid: function (str) { return RegExp(/^\d{9}$/g).test(str); }
 };
 var part2Optional = { cid: function (str) { return true; } };
+console.time("part 1");
+console.log(scanDocs(DATA, part2Required, part2Optional, true));
+console.timeLog("part 1");
 console.time("part 2");
-console.log(part2(DATA, part2Required, part2Optional));
+console.log(scanDocs(DATA, part2Required, part2Optional));
 console.timeLog("part 2");
--- advent-of-code/2020/day4/solution.ts
+++ advent-of-code/2020/day4/solution.ts
@@ -3,57 +3,60 @@ const fs = require("fs");
 
 const DATA = fs.readFileSync("./input.txt", "utf-8").split("\n\n");
 
-function part1(data: string[], requiredFields: string[], optionalFields: string[]) {
-  const rgx: RegExp = RegExp(`${requiredFields.map((field) => `(${field}:)`).join("|")}`, "gm");
+// function part1(data: string[], requiredFields: string[], optionalFields: string[]) {
+//   const rgx: RegExp = RegExp(`${requiredFields.map((field) => `(${field}:)`).join("|")}`, "gm");
 
-  return data.reduce((acc, doc) => {
-    if (doc.match(rgx).length >= requiredFields.length) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-}
+//   return data.reduce((acc, doc) => {
+//     if (doc.match(rgx).length >= requiredFields.length) {
+//       return acc + 1;
+//     }
+//     return acc;
+//   }, 0);
+// }
 
-// const fieldTitleRGX: RegExp = RegExp(`${Object.keys(requiredFields).map((field) => `(${field}:)`).join("|")}`, "g");
-
-function part2(data: string[], requiredFields: {}, optionalFields: {}) {
+function scanDocs(
+  data: string[],
+  requiredFields: {},
+  optionalFields: {} = {},
+  ignoreFieldValues: boolean = false,
+) {
   const requiredFieldKeys: Array<string> = Object.keys(requiredFields);
   const optionalFieldKeys: Array<string> = Object.keys(optionalFields);
 
   const validPassportCount = data.reduce((acc1, doc, i) => {
-    console.log("doc:", i);
-    console.log(doc);
-    const lines: string[] = doc.split(/ |\n/g);
-    if (requiredFieldKeys.length == lines.length || requiredFieldKeys.length + optionalFieldKeys.length == lines.length) {
-      const passingFieldsCount: number = lines.reduce(
-        (acc2, line) => {
-            const [key, value] = line.split(":");
-            // console.log(key, value);
-            if (requiredFieldKeys.includes(key) && requiredFields[key](value)) {
-              return acc2 + 1;
-            } else {
-              return acc2;
-            }
+    // console.log("doc:", i);
+    // console.log(doc);
+    const lines: string[][] = doc.split(/ |\n/g).map((line) => line.split(":"));
+    if (
+      requiredFieldKeys.length == lines.length ||
+      requiredFieldKeys.length + optionalFieldKeys.length == lines.length
+    ) {
+      const passingFieldsCount: number = lines.reduce((acc2, line) => {
+        const [key, value] = line;
+
+        if (ignoreFieldValues && requiredFieldKeys.includes(key)) {
+          return acc2 + 1;
+        } else if (requiredFieldKeys.includes(key) && requiredFields[key](value)) {
+          return acc2 + 1;
+        } else {
+           return acc2;
+        }
       }, 0);
 
       if (passingFieldsCount == requiredFieldKeys.length) {
-        console.log("pass");
+        // console.log("pass");
         return acc1 + 1;
       }
     }
-    console.log("fail");
+    // console.log("fail");
     return acc1;
   }, 0);
 
   return validPassportCount;
 }
 
-const part1Required: Array<string> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-const part1Optional: Array<string> = ["cid"];
-
-console.time("part 1");
-console.log(part1(DATA, part1Required, part1Optional));
-console.timeLog("part 1");
+// const part1Required: Array<string> = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+// const part1Optional: Array<string> = ["cid"];
 
 const part2Required = {
   byr: (str: string): boolean => parseInt(str, 10) >= 1920 && parseInt(str, 10) <= 2002,
@@ -76,6 +79,10 @@ const part2Required = {
 };
 const part2Optional = { cid: (str) => true };
 
+console.time("part 1");
+console.log(scanDocs(DATA, part2Required, part2Optional, true));
+console.timeLog("part 1");
+
 console.time("part 2");
-console.log(part2(DATA, part2Required, part2Optional));
+console.log(scanDocs(DATA, part2Required, part2Optional));
 console.timeLog("part 2");
