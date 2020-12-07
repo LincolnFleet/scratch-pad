0de69c2be3235e3afcdec2f7693102c4016a48ff
Travis Martin <travisemerymartin@gmail.com>
2020-12-07 00:42:27 -0500

    part 2 progress

--- advent-of-code/2020/day6/solution.js
+++ advent-of-code/2020/day6/solution.js
@@ -5,7 +5,7 @@ var DATA = fs
     .split(/\n\n/g)
     .map(function (group) { return group.split(/\n/g); });
 function part1(data) {
-    var yesAnswerCounts = [];
+    var yesAnswerCount = 0;
     for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
         var group = data_1[_i];
         var yesAnswers = [];
@@ -17,10 +17,34 @@ function part1(data) {
             }
         }
         var yesAnswersUnique = new Set(yesAnswers);
-        yesAnswerCounts.push(yesAnswersUnique.size);
+        yesAnswerCount += yesAnswersUnique.size;
     }
-    return yesAnswerCounts.reduce(function (acc, count) { return acc + count; });
+    return yesAnswerCount;
+}
+function part2(data) {
+    var yesToAllCounts = 0;
+    for (var _i = 0, data_2 = data; _i < data_2.length; _i++) {
+        var group = data_2[_i];
+        var yesAnswers = {};
+        for (var _a = 0, group_2 = group; _a < group_2.length; _a++) {
+            var person = group_2[_a];
+            for (var _b = 0, person_2 = person; _b < person_2.length; _b++) {
+                var letter = person_2[_b];
+                yesAnswers[letter] ? (yesAnswers[letter] += 1) : (yesAnswers[letter] = 1);
+            }
+        }
+        console.log(group.length, group);
+        for (var question in yesAnswers) {
+            if (yesAnswers[question] == group.length) {
+                yesToAllCounts += yesAnswers[question];
+            }
+        }
+    }
+    return yesToAllCounts;
 }
 console.time("part 1");
 console.log(part1(DATA));
 console.timeLog("part 1");
+console.time("part 2");
+console.log(part2(DATA));
+console.timeLog("part 2");
--- advent-of-code/2020/day6/solution.ts
+++ advent-of-code/2020/day6/solution.ts
@@ -7,7 +7,7 @@ const DATA: string[][] = fs
   .map((group) => group.split(/\n/g));
 
 function part1(data: string[][]): number {
-  const yesAnswerCounts: number[] = [];
+  let yesAnswerCount: number = 0;
 
   for (let group of data) {
     const yesAnswers: string[] = [];
@@ -17,12 +17,36 @@ function part1(data: string[][]): number {
       }
     }
     const yesAnswersUnique: Set<string> = new Set(yesAnswers);
-    yesAnswerCounts.push(yesAnswersUnique.size);
+    yesAnswerCount += yesAnswersUnique.size;
   }
 
-  return yesAnswerCounts.reduce((acc, count) => acc + count);
+  return yesAnswerCount;
+}
+
+function part2(data: string[][]): number {
+  let yesToAllCounts: number = 0;
+
+  for (let group of data) {
+    const yesAnswers: any = {};
+    for (let person of group) {
+      for (let letter of person) {
+        yesAnswers[letter] ? (yesAnswers[letter] += 1) : (yesAnswers[letter] = 1);
+      }
+    }
+    console.log(group.length, group);
+    for (let question in yesAnswers) {
+      if (yesAnswers[question] == group.length) {
+        yesToAllCounts += yesAnswers[question];
+      }
+    }
+  }
+  return yesToAllCounts;
 }
 
 console.time("part 1");
 console.log(part1(DATA));
 console.timeLog("part 1");
+
+console.time("part 2");
+console.log(part2(DATA));
+console.timeLog("part 2");
